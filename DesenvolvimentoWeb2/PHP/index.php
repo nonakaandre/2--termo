c85f22fb5c00100d6e0e7da7bf31ac64667cf0ae
André Nonaka <nonaka.andre@gmail.com>
2026-03-09 08:51:44 -0300

    commit

---
+++ DesenvolvimentoWeb2/PHP/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu site</title>
+</head>
+<body>
+    <h1>Meu site</h1>
+    <?php
+    echo "Impresso com PHP!"
+    ?>
+
+    <h2>Exemplos em PHP</h2>
+    <ul>
+        <li>
+            <a href="alomundo.php">Alô Mundo e uso de variaveis</a>
+        </li>
+    </ul>
+    
+</body>
+</html>
